039698251ae262690040fd26521752416f6e80df
Adrien Soursou <26015179+ChuOkupai@users.noreply.github.com>
2019-09-10 16:03:13 +0200

    Init

---
+++ get_next_line.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/10 14:56:36 by asoursou          #+#    #+#             */
+/*   Updated: 2019/09/10 16:02:23 by asoursou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+int			get_next_line(const int fd, char **line)
+{
+	static t_list	*l = NULL;
+	t_list			*e;
+	t_file			f;
+}
---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/10 14:56:36 by asoursou          #+#    #+#             */
+/*   Updated: 2019/09/10 15:17:39 by asoursou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft.h"
+# define BUFF_SIZE 32
+
+typedef struct	s_file
+{
+	char	*buf;
+	int		fd;
+}				t_file;
+
+int				get_next_line(const int fd, char **line);
+
+#endif
